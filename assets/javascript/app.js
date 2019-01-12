97496ea554074ecfc018ba3ce85359fba5c1823c
Anh Duong <a.dng223@gmail.com>
2019-01-11 22:12:18 -0600

    updated questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,24 +7,38 @@ document.getElementById("answers").style.display = 'none';
 var myQuestions = [
     {
         question: "What is the capital of Texas?",
-        answers: ['Dallas', 'Houston', 'Austin', 'San Antonio'],
-        correctAnswer: ['Austin']
+        answersA: 'Dallas',
+        answersB: 'Houston',
+        answersC: 'Austin',
+        answersD: 'San Antonio',
+        correctAnswer: ['Austin'],
+        image: "assets/images/austin.jpg"
     },
     {
         question: "What is the capital of the United States",
-        answers: ['Dallas', 'Washington D.C', 'New York', 'Los Angeles'],
-        correctAnswer: ['Washington D.C']
+        answersA: 'Dallas',
+        answersB: 'Los Angeles',
+        answersC: 'New York City',
+        answersD: 'Washington D.C',
+        correctAnswer: ['Washington D.C'],
+        image: "assets/images/dc.jpg"
     }
 ];
 
 var runningQuestion = 0; //current question
 var count = 0;
 var ans = $("#answers");
-var userAnswer= [];
+var userAnswer = [];
 var correct = 0;
-var counter = 15;
+var incorrect = 0;
+var timer = 15;
+var intervalId;
+var running = false;
 
-
+var choiceA = document.getElementById("a");
+var choiceB = document.getElementById("b");
+var choiceC = document.getElementById("c");
+var choiceD = document.getElementById("d");
 
 
 
@@ -32,29 +46,36 @@ function renderQuestions() {
     var q = myQuestions[runningQuestion];
 
     quest = document.getElementById("qst");
-            quest.innerHTML = q.question;
-    
+    quest.innerHTML = q.question;
+
 }
 function renderButtons() {
-    event.preventDefault();
     var q = myQuestions[runningQuestion];
-
-    for (var i = 0; i < q.answers.length; i++) {
-        var btn = document.createElement("p");
-        btn.setAttribute('id', 'ans');
-        btn.setAttribute('onClick', 'myFunction(this.innerHTML)');
-        var t = document.createTextNode(q.answers[i]);
-        btn.appendChild(t);
-        ans.append(btn);
-    }
+    choiceA.innerHTML = q.answersA;
+    choiceB.innerHTML = q.answersB;
+    choiceC.innerHTML = q.answersC;
+    choiceD.innerHTML = q.answersD;
 }
-function myFunction(clicked_id){ //store answer to empty array
+function myFunction(clicked_id) { //store answer to empty array
     event.preventDefault();
     var q = myQuestions[runningQuestion];
 
     var uGuess = clicked_id;
-    if(uGuess == q.correctAnswer){
+    if (uGuess == q.correctAnswer) {
+        stop();
         userWin();
+
+        runTimer();
+
+    }
+    else{
+        stop();
+        
+        lScreen();
+
+        reset();
+        userLoss()
+        runTimer();
     }
 }
 function userWin() {
@@ -62,34 +83,63 @@ function userWin() {
     runningQuestion++;
 
     renderQuestions();
+    renderButtons();
+
+}
+setTimeout(function userLoss() {
+    incorrect++;
+    runningQuestion++;
+
+    renderQuestions();
+    renderButtons();
+
+}, 5000);
+
+function lScreen() {
+    var q = myQuestions[runningQuestion];
+    quest = document.getElementById("qst");
+    quest.innerHTML = "Sorry! The correct answer is " + q.correctAnswer;
+    $("#answers").append(`<img src = ${q.image} >`);
 }
 
 
+function runTimer() {
+    if (!running) {
+        intervalId = setInterval(decrement, 1000);
+        running = true;
+    }
+}
 
+function decrement() {
+    $("#timer").html(timer);
+    timer--;
+
+    //stop timer if reach 0
+    if (timer === 0) {
+        //unanswerCount++;
+        stop();
+        $("#time").html("Time is up!");
+
+    }
+}
+function stop() {
+    running = false;
+    clearInterval(intervalId);
+}
 
 $("#start").click(function () {
     $("#start").hide();
     $('#time').show();
     $('#question').show();
     $('#answers').show();
-    
-    setInterval(function () {
-        counter--;
-        if (counter >= 0) {
-            span = document.getElementById("timer");
-            span.innerHTML = counter;
-        }
-        if (counter === 0) {
-            $('#time').html('out of time!');
-            
-            clearInterval(counter);
-        }
-    }, 1000);
 
-    setInterval();
+    runTimer();
     renderQuestions();
     renderButtons();
-
 });
 
+function reset(){
+    $("#answers").empty();
+
+}
 
